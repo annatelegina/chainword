2ca56d8afc887f679b8136d52d422dc9593b89c3
Anna <anya519@mail.ru>
2019-04-09 00:02:27 +0300

    add reading

--- src/MyProj.hs
+++ src/MyProj.hs
@@ -5,6 +5,7 @@ module MyProj
 import Graphics.Gloss
 import Prelude
 import Graphics.Gloss.Interface.Pure.Game
+import System.IO
 sizeWin = 560 :: Int
 centPos  = 100 :: Int
 
@@ -18,11 +19,11 @@ data GameState = Error | Ok
 data World = World
     { field :: [Maybe Char]
     , answers :: [Letter]
-    --, final :: String
     , marker :: Int
     , colour_marker :: Color
     , colour_letter :: Color
     , colour_number :: Color
+    , state :: Int --0 - playing, 1 - empty cells, 2 - end
     }
 tmpField = [ (a, 1)| a <-[8,7..1]] ++ [(b, 2) |b <-[1,2..8]] ++ [ (a, 3)| a <-[8,7..1]] 
            ++ [(b, 4) |b <-[1,2..8]] ++ [ (a, 5)| a <-[8,7..1]] ++ [(b, 6) |b <-[1,2..8]] 
@@ -64,7 +65,7 @@ position :: (Int, Int) -> (Float, Float)
 position (x, y) = (fromIntegral(- div sizeWin 2 + y*cellSize), fromIntegral(div (sizeWin+heiOffset) 2 - x*cellSize))
 
 makeWorld :: [String] -> World
-makeWorld x = World (replicate (cellDim*cellDim) Nothing) (concatList 1 x) 1 green black red
+makeWorld x = World (replicate (cellDim*cellDim) Nothing) (concatList 1 x) 1 green black red 0
 
 drawMarker:: Int -> Picture
 drawMarker n = translate (fst cord) (snd cord) $ color green $ rectangleSolid size size
@@ -73,15 +74,15 @@ drawMarker n = translate (fst cord) (snd cord) $ color green $ rectangleSolid si
                      cord = getCenter (getCell n) 
 	   
 drawWorld :: World -> Picture
-drawWorld (World x y num colour lett numcol) = (base<>marker<>words<>numbers)
-                                where
-                                     base = (makeVertical cellDim) <> (makeHorizontal cellDim) <> chainLines
-                                     marker = drawMarker num
-                                     words = mconcat [color lett $ translate (fst $ getCenter (getCell n)) (snd $ getCenter (getCell n)) $ scale 0.2 0.2 $ Text $ show ch | n<- [1..length x], Just ch <- [x!!(n-1)]]
-                                     numbers = mconcat [color numcol $ translate (fst $ getCorner (getCell n)) (snd $ getCorner (getCell  n)) $ scale 0.1 0.1 $ Text $ show k | n<- [1..length cell], Just k<- [cell!!(n-1)] ]
-                                     cell = getNum y
-
-
+drawWorld (World x y num colour lett numcol 0)  = (base<>marker<>words<>numbers)
+                                                    where
+                                                    base = (makeVertical cellDim) <> (makeHorizontal cellDim) <> chainLines
+                                                    marker = drawMarker num
+                                                    words = mconcat [color lett $ translate (fst $ getCenter (getCell n)) (snd $ getCenter (getCell n)) $ scale 0.2 0.2 $ Text $ show ch | n<- [1..length x], Just ch <- [x!!(n-1)]]
+                                                    numbers = mconcat [color numcol $ translate (fst $ getCorner (getCell n)) (snd $ getCorner (getCell  n)) $ scale 0.1 0.1 $ Text $ show k | n<- [1..length cell], Just k<- [cell!!(n-1)] ]
+                                                    cell = getNum y
+drawWorld (World x y num colour lett numcol 1) = scale 0.15 0.15 $ color red $ Text $ endOfGame x y
+drawWorld (World x y num colour lett numcol 2) = scale 0.15 0.15 $ color red $ Text $ endOfGame x y
 
 getNum :: [Letter] -> [Maybe Int]
 getNum [] = []
@@ -127,6 +128,10 @@ showLst [] = "The end."
 showLst (Just x : xs) = "Char" ++(show x) ++ "\n" ++ showLst xs
 showLst (Nothing : xs) = "Nothing" ++ "\n"++ showLst xs
 
+showQuest :: [String] -> String
+showQuest [] = "."
+showQuest (x:xs) = (show x) ++ "\n" ++ showQuest xs
+
 --action for insert/delete symbols in the chain
 smthLetter :: Action -> Int  -> Char -> [Maybe Char] -> [Maybe Char]
 smthLetter Delete 1 _ (x:xs) = Nothing : xs
@@ -139,11 +144,15 @@ smthLetter Insert n s (x:xs) = x : smthLetter Insert (n-1) s xs
 fromJust :: Maybe a -> a
 fromJust (Just x) = x
 
-checkRight :: (Eq a) => [a]->[Maybe a] -> Int
+checkRight :: (Eq a)=> [a]->[Maybe a] -> Int
 checkRight [] _ = 0
 checkRight (x:xs) (Nothing : ys) = checkRight xs ys
 checkRight (x:xs) (y:ys) = if x ==  (fromJust y) then (1 + checkRight xs ys)  else checkRight xs ys
 
+checkEnd :: (Eq a) => [Maybe a] -> Bool
+checkEnd [] = True
+checkEnd (Nothing : xs) = False
+checkEnd (x : xs) = checkEnd xs
 
 endOfGame :: [Maybe Char] -> [Letter] -> String
 endOfGame [] _ = "Well done!\n"
@@ -206,41 +215,42 @@ drawing = pictures
 --playing game
 handleEvent :: Event -> World -> World
 
-handleEvent (EventKey (SpecialKey KeyEnter) Down _ _) (World x y num colour lett numcol) = World x y num colour lett numcol
-                              
-handleEvent (EventKey (SpecialKey KeyRight) Down _ _) (World x y num colour lett numcol) 
-                                                                                | n < 8 = World x y newNum colour lett numcol 
-                                                                                | otherwise = World x y num colour lett numcol      
+handleEvent (EventKey (SpecialKey KeyEnter) Down _ _) (World x y num colour lett numcol 0)| (checkEnd x) == False = World x y num colour lett numcol 1
+                                                                                          | otherwise = World x y num colour lett numcol 2
+handleEvent (EventKey (SpecialKey KeyEnter) Down _ _) (World x y num colour lett numcol 1) = World x y num colour lett numcol 0
+handleEvent (EventKey (SpecialKey KeyRight) Down _ _) (World x y num colour lett numcol fin) 
+                                                                                | n < 8 = World x y newNum colour lett numcol fin
+                                                                                | otherwise = World x y num colour lett numcol fin
                                                                                 where 
                                                                                     n = snd point
                                                                                     newNum = getMarker (fst point, snd point  + 1)
                                                                                     point = getCell num  
     
-handleEvent (EventKey (SpecialKey KeyLeft) Down _ _) (World x y num colour lett numcol) 
-                                                                                | n > 1 = World x y newNum colour lett numcol 
-                                                                                | otherwise = World x y num colour lett numcol      
+handleEvent (EventKey (SpecialKey KeyLeft) Down _ _) (World x y num colour lett numcol fin) 
+                                                                                | n > 1 = World x y newNum colour lett numcol fin
+                                                                                | otherwise = World x y num colour lett numcol fin
                                                                                 where 
                                                                                     n = snd point
                                                                                     newNum = getMarker (fst point, snd point  - 1)
                                                                                     point = getCell num 
-handleEvent (EventKey (SpecialKey KeyUp) Down _ _) (World x y num colour lett numcol) 
-                                                                                | n > 1 = World x y newNum colour lett numcol 
-                                                                                | otherwise = World x y num colour lett numcol      
+handleEvent (EventKey (SpecialKey KeyUp) Down _ _) (World x y num colour lett numcol fin) 
+                                                                                | n > 1 = World x y newNum colour lett numcol fin
+                                                                                | otherwise = World x y num colour lett numcol fin
                                                                                 where 
                                                                                     n = fst point
                                                                                     newNum = getMarker (fst point - 1, snd point)
                                                                                     point = getCell num 
-handleEvent (EventKey (SpecialKey KeyDown) Down _ _) (World x y num colour lett numcol) 
-                                                                                | n < 8 = World x y newNum colour lett numcol 
-                                                                                | otherwise = World x y num colour lett numcol      
+handleEvent (EventKey (SpecialKey KeyDown) Down _ _) (World x y num colour lett numcol fin) 
+                                                                                | n < 8 = World x y newNum colour lett numcol fin
+                                                                                | otherwise = World x y num colour lett numcol fin
                                                                                 where 
                                                                                     n = fst point
                                                                                     newNum = getMarker (fst point + 1, snd point)
                                                                                     point = getCell num
-handleEvent (EventKey (SpecialKey KeySpace) Down _ _) (World x y num colour lett numcol) = World newAns y num colour lett numcol
+handleEvent (EventKey (SpecialKey KeySpace) Down _ _) (World x y num colour lett numcol fin) = World newAns y num colour lett numcol fin
                                                                        where
                                                                            newAns = smthLetter Delete num 'a' x
-handleEvent (EventKey (Char sym) Down _ _) (World x y num colour lett numcol) = World newAns y num colour lett numcol
+handleEvent (EventKey (Char sym) Down _ _) (World x y num colour lett numcol fin) = World newAns y num colour lett numcol fin
                                                                        where
                                                                            newAns = smthLetter Insert num sym x
 handleEvent _ w = w
@@ -257,6 +267,21 @@ test1 = ["teleging", "gnomes", "separate", "equal", "looking"] :: [String]
 stepsPerSecond :: Int
 stepsPerSecond = 60
 
+find:: (Eq a) => [a] -> a -> Int
+find xs e 
+    | elem e xs = ffind xs e 
+    | otherwise = length xs 
+    where 
+        ffind (x:xs) e
+                | x == e    = 0
+                | otherwise     = 1 + ffind xs e
+parseInput:: String -> [String]
+parseInput s
+    | null s = [] 
+    | otherwise= (take (find s '\n') s): (parseInput (drop ((find s '\n')+1) s))
+    
+quest = "./test/questions.txt" :: String
+ans = "./test/answers.txt" :: String
 
 runMyProj :: IO ()
 runMyProj = do
@@ -273,7 +298,10 @@ runMyProj = do
                    -- s =  concatList 1 test1
                    -- d = smthLetter Insert 1 5 "abcde" a
            -- putStrLn (show (getCenter (getCell 1)) )
-           let initState = makeWorld test1
+           s <- readFile quest
+           putStrLn s
+           b <- readFile ans
+           let initState = makeWorld $ parseInput b
            play window background stepsPerSecond initState drawWorld handleEvent update
            --play display window background (drawWorld initState) handleEvent update
            --play display bgColor stepsPerSecond initState drawWorld handleEvent update
